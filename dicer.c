be67bc74fc830b9fde12be93735bb8931bfe056c
Shawn G <shawn.gottman@gmail.com>
2015-03-21 00:58:26 -0700

    moved struct def. from dicer.c to dicer.h

--- dicer.c
+++ dicer.c
@@ -15,13 +15,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-*/
+
 struct roll{
     int arraySize, addedValue;
     int *arrayOfValues;
     struct roll *next;
 };
-/*
+
 int syntax(char *input, struct roll ***head, int commaCount);
 void cleanSpaces(char *input, int len);
 void randNums(struct roll *temp, int modulus);
